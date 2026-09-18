8eae0ef9c67b883392be0b5a538ecc08827b7ac3
Matthew Brett <matthew.brett@gmail.com>
2026-09-18 12:14:02 +0100

    Pandoc PDF output template for nbconvert

---
+++ jupyter_nbconvert_config.py
@@ -0,0 +1,11 @@
+"""Project nbconvert config: LaTeX/PDF template for pandoc longtables."""
+
+from pathlib import Path
+
+c = get_config()  # noqa: F821
+
+_TEMPLATES = str(Path(__file__).resolve().parent / "nbconvert_templates")
+
+c.TemplateExporter.extra_template_basedirs = [_TEMPLATES]
+c.LatexExporter.template_name = "latex_pandoc_tables"
+c.PDFExporter.template_name = "latex_pandoc_tables"
