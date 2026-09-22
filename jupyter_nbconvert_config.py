a59b40fe8deaf9f744b4a4db2a7b01cd24d8e786
Stefan van der Walt <stefanv@berkeley.edu>
2026-09-21 18:41:10 -0700

    Mark nbconvert_templates private

--- jupyter_nbconvert_config.py
+++ jupyter_nbconvert_config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 c = get_config()  # noqa: F821
 
-_TEMPLATES = str(Path(__file__).resolve().parent / "nbconvert_templates")
+_TEMPLATES = str(Path(__file__).resolve().parent / "_nbconvert_templates")
 
 c.TemplateExporter.extra_template_basedirs = [_TEMPLATES]
 c.LatexExporter.template_name = "latex_pandoc_tables"
